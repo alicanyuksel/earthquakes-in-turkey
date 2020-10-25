9389c7820aa8f3e2a414c8eed716dd658b3db1b5
Alican Yüksel <alicanyuksel@outlook.com>
2020-10-25 17:11:00 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from tools import extract_from_web
 from tools import clean_data
 from tools import create_json_file
 
+
 def run(file_name):
     extract_from_web(file_name=file_name)
     clean_data(file_name=file_name)
